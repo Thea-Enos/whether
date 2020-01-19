872561453e6800ab6144ac7b20044c52e1e972b6
Thea <thea@climbcred.it>
2020-01-19 00:39:25 -0500

    ENH initialize

---
+++ src/components/Section/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Section = () => (
+  <section>
+    <div>
+      <div>
+        This container is <strong>centered</strong> on desktop.
+      </div>
+    </div>
+  </section>
+);
+
+export default Section;
